e25cb41c3a559166baf63152864baaa4c019b1cd
tribore <5824063+trib0r3@users.noreply.github.com>
2023-06-14 20:40:53 +0200

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # iosBackupReconstruct
-Restructure the iOS backup directory from GUID directories to human-readable
+
+Reconstruct the human-readable file structure from decrypted guid-based iOS Backup file structure.
+
+## Usage
+```bash
+python3 iosBackupRestructure.py ./decrypted-backup ./output
+```
+
+## How it works
+Script reads the contents of `Manifest.db` file and writes:
+- files (type `flags = 1`) under paths in `relativePath` column
+- meta-files (column `file`, `flags !=1 `) are stored in directories provided under `relativePath` in files with name: `meta-domain.flags.plist`
+
---
+++ iosBackupRestructure.py
@@ -0,0 +1,54 @@
+from sys import argv
+import os
+import sqlite3
+import shutil
+
+def process_backup(backup_dir: str, output_dir: str) -> None:
+    manifest_path = os.path.join(backup_dir, "Manifest.db")
+    if not os.path.exists(manifest_path):
+        print("Can't find Manifest.db in backup dir!")
+        return
+    
+    db_connection = sqlite3.connect(manifest_path)
+    db_cursor = db_connection.cursor()
+    file_list = db_cursor.execute("select fileID, relativePath from Files where flags=1").fetchall()
+    for entry in file_list:
+        file_id, relative_file_path = entry
+        relative_dir = os.path.dirname(relative_file_path)
+        os.makedirs(os.path.join(output_dir, relative_dir), exist_ok=True)
+        
+        original_path = os.path.join(backup_dir, file_id[:2], file_id)
+        destination_path = os.path.join(output_dir, relative_file_path)
+        shutil.copy2(src=original_path, dst=destination_path)
+    print("[*] Copied Files to destination path!")
+
+    metafiles = db_cursor.execute("select fileID, relativePath, domain, file, flags from Files where flags!=1").fetchall()
+    for entry in metafiles:
+        file_id, relative_path, domain, file_data, flags = entry
+        relative_dir = os.path.dirname(relative_path)
+        os.makedirs(os.path.join(output_dir, relative_dir), exist_ok=True)
+        fname = f"meta-{domain}.{flags}.plist"
+        destination_path = os.path.join(output_dir, relative_dir, fname)
+        with open(destination_path, "wb") as f:
+            f.write(file_data)
+    print("[*] Saved Meta-Files to destination path!")
+    db_connection.close()
+
+
+def main() -> None:
+    if len(argv) != 3:
+        print(f"USAGE: ./{argv[0]} /path/to/decrypted/backup /output/directory")
+        return
+    
+    backup = argv[1]
+    output = argv[2]
+    if not os.path.exists(backup) or os.path.exists(output) and len(os.listdir(output)) > 0:
+        print("Invalid backup directory or not empty:", backup)
+        return
+    
+    os.makedirs(output, exist_ok=True)
+    process_backup(backup_dir=backup, output_dir=output)
+
+
+if __name__ == "__main__":
+    main()
